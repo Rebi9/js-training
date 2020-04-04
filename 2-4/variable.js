7171f58e3c455ca6270a74c707b040e463df4ecc
ryo_ebihara <ab.yr.2t@gmail.com>
2020-04-04 13:40:48 +0900

    feat:variable

---
+++ 2-4/variable.js
@@ -0,0 +1,19 @@
+//　変数の定義
+let message = "Hello";
+
+alert(message);
+
+// 複数の変数宣言も可能だが、可読性的に1変数1行の方が良い。
+let user = "John",
+  age = 25,
+  message = "Hello";
+
+let user = "paul",
+  age = 23,
+  message = "hey";
+
+// 定数の定義
+const myBirthday = "18.04.1982";
+
+// myBirthdayは定数なので、再代入しようとするとエラー
+// myBirthday = '01.01.2001'
