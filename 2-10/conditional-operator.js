fa76488e0725bcb1ca9739d4a0dbaa158b0a6d31
ryo_ebihara <ab.yr.2t@gmail.com>
2020-04-10 19:10:59 +0900

    feat:conditional operator

---
+++ 2-10/conditional-operator.js
@@ -0,0 +1,16 @@
+// if文
+if ((year = 2020)) {
+  alert("That's collect");
+}
+
+if (year < 2020) {
+  alert("Too early...");
+} else if (year > 2020) {
+  alert("Too late...");
+} else {
+  alert("Exactly!");
+}
+
+// 3項演算子
+// 条件式に括弧はなくても良い。
+let courseName = price >= 10000 ? "高級コース" : "一般コース";
